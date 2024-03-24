e852e55190eddf2ff134ebe1985daaebd56beae3
Ben Muschol <benmuschol@gmail.com>
2024-03-24 08:23:05 -0400

    Pico css

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import '@picocss/pico'
+</script>
+
+<svelte:head>
+  <title>Ana: Simple Anagramming</title>
+</svelte:head>
+
+
+<main class="container">
+  <slot/>
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Ana</h1>
+<p>The simpler anagramming tool. Coming <strike>soon</strike> eventually.</p>
